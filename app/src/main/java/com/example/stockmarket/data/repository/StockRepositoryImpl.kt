d1ae77ff237b22e4af2300b27fbb5d2379a4a9b6
Misa <milosursulovic2696@gmail.com>
2022-12-26 17:49:07 +0100

    Implement repository

---
+++ app/src/main/java/com/example/stockmarket/data/repository/StockRepositoryImpl.kt
@@ -0,0 +1,50 @@
+package com.example.stockmarket.data.repository
+
+import com.example.stockmarket.data.local.StockDatabase
+import com.example.stockmarket.data.mapper.toCompanyListing
+import com.example.stockmarket.data.remote.StockApi
+import com.example.stockmarket.domain.model.CompanyListing
+import com.example.stockmarket.domain.repository.StockRepository
+import com.example.stockmarket.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class StockRepositoryImpl @Inject constructor(
+    val api: StockApi,
+    db: StockDatabase
+) : StockRepository {
+
+    private val dao = db.dao
+
+    override suspend fun getCompanyListing(
+        fetchFromRemote: Boolean,
+        query: String
+    ): Flow<Resource<List<CompanyListing>>> = flow {
+        emit(Resource.Loading(true))
+        val localListings = dao.searchCompanyListing(query)
+        emit(Resource.Success(localListings.map { it.toCompanyListing() }))
+
+        val isDbEmpty = localListings.isEmpty() && query.isBlank()
+        val shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote
+
+        if (shouldJustLoadFromCache) {
+            emit(Resource.Loading(false))
+            return@flow
+        }
+
+        val remoteListings = try {
+            val response = api.getListings()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            emit(Resource.Error("Couldn't load data"))
+        } catch (e: HttpException) {
+            e.printStackTrace()
+            emit(Resource.Error("Couldn't load data"))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/stockmarket/util/Resource.kt
@@ -0,0 +1,10 @@
+package com.example.stockmarket.util
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Loading<T>(val isLoading: Boolean = true) : Resource<T>(null)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+}
